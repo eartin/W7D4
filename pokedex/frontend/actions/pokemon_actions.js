33de6cff013dd3668b02c604a0528afcb0598566
eartin <eugeneartin@gmail.com>
2019-06-27 11:52:50 -0700

    Implement store, thunk, requestAllPokemon action

--- pokedex/frontend/actions/pokemon_actions.js
+++ pokedex/frontend/actions/pokemon_actions.js
@@ -4,3 +4,8 @@ export const receiveAllPokemon = pokemon => ({
   type: RECEIVE_ALL_POKEMON,
   pokemon
 })
+
+export const requestAllPokemon = () => (dispatch) => (
+  fetchAllPokemon()
+    .then(pokemon => dispatch(receiveAllPokemon(pokemon)))
+)
--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {fetchAllPokemon} from './util/api_util';
-import {RECEIVE_ALL_POKEMON, receiveAllPokemon } from './actions/pokemon_actions';
-
+import {RECEIVE_ALL_POKEMON, receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
+  const store  = configureStore();
   window.fetchAllPokemon = fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
