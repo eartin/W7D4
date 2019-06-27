c8df9f803522ba1c1c789223c6a4945a4e8d16c2
eartin <eugeneartin@gmail.com>
2019-06-27 12:12:48 -0700

    Implement selectAllPokemon selector

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import {fetchAllPokemon} from './util/api_util';
 import {RECEIVE_ALL_POKEMON, receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
 import configureStore from './store/store';
+import { selectAllPokemon } from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   const store  = configureStore();
+  
   window.fetchAllPokemon = fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
   window.requestAllPokemon = requestAllPokemon;
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  window.selectAllPokemon = selectAllPokemon;
+
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
